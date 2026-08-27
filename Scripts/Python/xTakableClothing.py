3a36039633a611ac9a3bf88339510d3f2b57f690
Mystler <161936+Mystler@users.noreply.github.com>
2026-08-27 21:40:14 +0200

    Minor improvement

--- Scripts/Python/xTakableClothing.py
+++ Scripts/Python/xTakableClothing.py
@@ -359,12 +359,12 @@ class xTakableClothing(ptModifier):
         if wearNow:
             self.IRemoveWornSet(self.IConflictsWithSet(base))
         worn = avatar.avatar.getAvatarClothingList()
-        wornnamelist = [i[0] for i in worn]
+        wornnamelist = frozenset([i[0] for i in worn])
         changeGuildShirt = False
         if base.find('Torso_GuildBlue') != -1 or base.find('Torso_GuildGreen') != -1 or base.find('Torso_GuildRed') != -1 or base.find('Torso_GuildYellow') != -1 or base.find('Torso_GuildWhite') !=  -1:
             changeGuildShirt = any((guildShirt in wornnamelist for guildShirt in guildSDLValues))
             if changeGuildShirt:
-                    PtDebugPrint("xTakableClothing: Found worn guild short, enforcing swap", level=kWarningLevel)
+                PtDebugPrint("xTakableClothing: Found worn guild short, enforcing swap", level=kWarningLevel)
             self.IRemoveOtherGuildShirt()
             psnlSDL = xPsnlVaultSDL()
             psnlSDL["guildAlliance"] = (guildSDLValues[base],)
